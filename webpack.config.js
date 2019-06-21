ffbd9cee67699f9f681c01a3372f2c880181c0b5
Nhan Do <nhan.do@niteco.se>
2019-06-21 17:13:35 +0700

    config mode env

--- webpack.config.js
+++ webpack.config.js
@@ -3,54 +3,65 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  name: 'Template Webpack',
-  mode: 'development',
-  entry: ['./src/scripts/main.js','./src/styles/main.scss' ],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[hash].bundle.js'
-  },
-  devServer: {
-    hot: true,
-    open: true,
-    port: 3001,
-    watchContentBase: true,
-    watchOptions: {
-      ignored: /node_modules/
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.pug$/,
-        use: ["pug-loader"]
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        })
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          {
-            loader: 'file-loader'
-          }
-        ]
+module.exports = function(env, argv) {
+  const isProduction = argv.mode === 'production';
+  return {
+    name: 'Template Webpack',
+    mode: isProduction ? 'production' : 'development',
+    entry: ['./src/scripts/main.js','./src/styles/main.scss' ],
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: `${isProduction ? [hash] + '.' : ''}bundle.js`
+    },
+    devServer: {
+      hot: true,
+      open: true,
+      port: 3001,
+      watchContentBase: true,
+      watchOptions: {
+        ignored: /node_modules/
       }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.pug$/,
+          use: [
+            {
+              loader: "pug-loader",
+              query: {
+                pretty: false
+              }
+            }
+          ],
+        },
+        {
+          test: /\.scss$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader', 'sass-loader']
+          })
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+            {
+              loader: 'file-loader'
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebPackPlugin({
+        template: './src/views/index.pug',
+        inject: true,
+      }),
+      new ExtractTextPlugin({
+        filename: `${isProduction ? [hash] + '.' : ''}bundle.css`,
+        allChunks: true,
+      }),
     ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebPackPlugin({
-      template: './src/views/index.pug'
-    }),
-    new ExtractTextPlugin({
-      filename: '[hash].bundle.css',
-      allChunks: true,
-    }),
-  ]
+  };
 };
